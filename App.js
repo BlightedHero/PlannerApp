8fbb23b4fdc161b1eb3614711cc8ad484ff0edb8
San Yu <0428sanyu@gmail.com>
2023-08-06 13:33:07 -0700

    Post Screen Example

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, TextInput, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({navigation, route}) {
 
   const {itemId} = route.params;
+
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -15,6 +22,11 @@ function HomeScreen({navigation, route}) {
         title="Go to Menu"
         onPress={() => navigation.push('Menu', {itemId: 5})}
       />
+       <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
@@ -33,13 +45,43 @@ function MenuScreen({navigation, route}) {
       />
       <Button
         title="Change Title"
+        //setOptions used for screen shit
         onPress={() => navigation.setOptions({title: Math.floor(Math.random() * 100).toString()})}
       />
       <Button
         title="Change itemId"
+        //setParams used for non screen shit
         onPress={() => navigation.setParams({itemId: Math.floor(Math.random() * 100)})}
       />
     </View>
+  );
+}
+
+// Creates seperate screen allowing params to be sent through
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
   );
 }
 
@@ -63,6 +105,11 @@ function App() {
           options={{ title: 'Overview' }} 
           initialParams={{ itemId: 17 }}
         />
+        <Stack.Screen 
+          name="CreatePost" 
+          component={CreatePostScreen}
+        />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
