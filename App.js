87be20b25abf1bafde1cba68e9f337f8d4aa33d5
San Yu <0428sanyu@gmail.com>
2023-08-06 12:58:50 -0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({navigation, route}) {
+
+  const {itemId} = route.params;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Button
+        title="Go to Menu"
+        onPress={() => navigation.push('Menu', {itemId: 5})}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MenuScreen({navigation, route}) {
+
+  const {itemId} = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Menu Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Button
+        title="Go to Menu"
+        onPress={() => navigation.push('Menu', {itemId: 7})}
+      />
+      <Button
+        title="Change Title"
+        onPress={() => navigation.setOptions({title: Math.floor(Math.random() * 100).toString()})}
+      />
+      <Button
+        title="Change itemId"
+        onPress={() => navigation.setParams({itemId: Math.floor(Math.random() * 100)})}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Overview' }} 
+          initialParams={{ itemId: 42 }}
+        />
+
+        <Stack.Screen 
+          name="Menu" 
+          component={MenuScreen}
+          options={{ title: 'Overview' }} 
+          initialParams={{ itemId: 17 }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
