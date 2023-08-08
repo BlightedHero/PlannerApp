1bb3387b8dac3c93cbfc46d00c2d1b8dd0284c4c
anjx123 <114792511+anjx123@users.noreply.github.com>
2023-08-08 14:44:52 -0700

    Implemented Drawer Navigation
    Downloaded a couple of dependencies and shit so it may break :thumbsup:

--- navigation/navigationController.js
+++ navigation/navigationController.js
@@ -3,33 +3,47 @@ import { Button, TextInput, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen, MenuScreen, CreatePostScreen } from '../App';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function NavigationController() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
+            <Stack.Navigator>
                 <Stack.Screen 
                 name="Home" 
-                component={HomeScreen} 
-                options={{ title: 'Overview' }} 
-                initialParams={{ itemId: 42 }}
+                component={DrawerNavigation} 
+                options={{headerShown: false}} 
                 />
-
                 <Stack.Screen 
                 name="Menu" 
-                component={MenuScreen}
-                options={{ title: 'Overview' }} 
-                initialParams={{ itemId: 17 }}
+                component={DrawerNavigation}
+                options={{headerShown: false}} 
                 />
                 <Stack.Screen 
                 name="CreatePost" 
                 component={CreatePostScreen}
                 />
-                
             </Stack.Navigator>
         </NavigationContainer>
+    );
+}
+
+function DrawerNavigation() {
+    return (
+        <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name="HomeScreen" 
+                component={HomeScreen} 
+                options={{ title: 'Overview' }} 
+                initialParams={{ itemId: 42 }}/>
+            <Drawer.Screen 
+                name="MenuScreen" 
+                component={MenuScreen}
+                options={{ title: 'MenuScreen' }} 
+                initialParams={{ itemId: 17 }}/>
+        </Drawer.Navigator>
     );
 }
